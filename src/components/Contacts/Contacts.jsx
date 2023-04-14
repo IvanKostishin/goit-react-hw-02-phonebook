f7cde196cf5ef2a48674768fb6fbcd660345f5cc
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-04-15 00:47:45 +0300

    create

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,37 @@
+import ContactsItem from 'components/ContactsItem/ContactsItem';
+import css from './Contacts.module.css';
+import propTypes from 'prop-types';
+
+const Contacts = ({ handleDelete, filter, contacts }) => {
+  return (
+    <>
+      <ul className={css.list}>
+        {filter
+          ? contacts
+              .filter(value => value.name.toLowerCase().includes(filter))
+              .map(contact => (
+                <ContactsItem
+                  key={contact.id}
+                  handleDelete={handleDelete}
+                  name={contact.name}
+                  number={contact.number}
+                ></ContactsItem>
+              ))
+          : contacts.map(contact => (
+              <ContactsItem
+                key={contact.id}
+                handleDelete={handleDelete}
+                name={contact.name}
+                number={contact.number}
+              ></ContactsItem>
+            ))}
+      </ul>
+    </>
+  );
+};
+export default Contacts;
+Contacts.propTypes = {
+  handleDelete: propTypes.func.isRequired,
+  filter: propTypes.string,
+  contacts: propTypes.array.isRequired,
+};
